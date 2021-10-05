27da9291059c6864ca7cfa23d7d077feb1abc62a
Nikolai H <nikolai.humphreyb@gmail.com>
2021-10-05 15:43:33 -0400

    calc: adjust UI

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,34 @@
+const display = document.getElementById('display')
+const digits = document.querySelectorAll('.digit')
+const operators = document.querySelectorAll('.operator')
+const clear = document.getElementById('clear')
+let displayValue = display.innerText
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', (e) => {
+        console.log('click')
+        e.preventDefault()
+        populateDisplay(digit.innerText)
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        e.preventDefault()
+        populateDisplay(operator.innerText)
+    })
+})
+
+clear.addEventListener('click', (e) => {
+    e.preventDefault()
+    display.innerHTML = '&nbsp'
+})
+
+function populateDisplay (val) {
+    displayValue = val
+    display.innerText += displayValue
+}
+
 function operate (num1, num2, operator) {
     if (operator = '+') {
         add(num1, num2)
